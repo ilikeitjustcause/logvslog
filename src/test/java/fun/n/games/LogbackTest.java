64b4be0d02a937f4868e5cb4e0a327351bec676f
ilikeitjustcause <ilikeitjustcause@gmail.com>
2015-10-06 22:39:40 +0530

    The first failing test.

---
+++ src/test/java/fun/n/games/LogbackTest.java
@@ -0,0 +1,14 @@
+package fun.n.games;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LogbackTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
